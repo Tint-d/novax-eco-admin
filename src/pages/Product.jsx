d1656707600f2322800f4c50f443940e58ca5aa9
GerVaf <thantzinmin667@gmail.com>
2023-06-26 21:41:26 +0630

    v1.5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 import Layout from "../Layout";
-import { useProductsListQuery } from '../services/api/product';
+import { useProductsListQuery } from "../services/api/product";
 
 const Product = () => {
-  const token = JSON.parse(localStorage.getItem('userInfo'));
-  console.log(token)
-  const { data,error } = useProductsListQuery(token)
-  console.log(data,error);
-  return (
-    <Layout>
+  const token = useSelector((state) => state.auth.token);
+  console.log(token);
 
-    </Layout>
-    )
-}
+  const { data : {data}, error } = useProductsListQuery(token);
+  // console.log(data, error);
 
-export default Product
+  const products = data?.data
+  console.log(products)
+  return <Layout>
+    <th>{products.map((el)=>{
+      return(
+        <li>{el.title}</li>
+      )
+    })}</th>
+  </Layout>;
+};
+
+export default Product;
